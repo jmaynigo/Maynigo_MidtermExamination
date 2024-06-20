20f608b697c45c33a374b11247905b6ec357fb58
jmaynigo <jpm2242@students.uc-bcf.edu.ph>
2024-06-20 18:57:22 +0800

    Creating routes for ProductController and movies.blade.php.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/movies', function () {
+    return view('movies');
+});
+
+Route::get('/movies', [ProductController::class, 'index'])->name('movies');
