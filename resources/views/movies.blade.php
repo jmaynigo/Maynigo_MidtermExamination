f3db28316779ea7aa403869e40cf875cee4dcb40
jmaynigo <jpm2242@students.uc-bcf.edu.ph>
2024-06-20 18:56:39 +0800

    Displaying data from ProductController in view.

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Maynigo Midterm Exam</title>
+        </head>
+        <body>
+            <h1>Movies</h1>
+            <h2>{{ $title1 }}</h2>
+            <p>Distributed by: {{ $distributor1 }}</p>
+            <p>Release Date: {{ $releaseDate1 }}</p>
+            <h2>{{ $title2 }}</h2>
+            <p>Distributed by: {{ $distributor2 }}</p>
+            <p>Release Date: {{ $releaseDate2 }}</p>
+            <h2>{{ $title3 }}</h2>
+            <p>Distributed by: {{ $distributor3 }}</p>
+            <p>Release Date: {{ $releaseDate3 }}</p>
+        </body>
+    </html>
+</div>
